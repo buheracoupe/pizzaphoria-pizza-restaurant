915c2e7755d728ff07058b3e2e07217c26234e2b
double <darrelltduri@gmail.com>
2024-10-31 22:47:12 +0200

    create routes animation with framer motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, useLocation } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import {Routes, Route} from "react-router-dom";
 import OurStory from './components/Pages/OurStory';
@@ -8,6 +8,51 @@ import Home from './components/Pages/Home';
 import Menu from './components/Pages/Menu';
 import GiftCards from './components/Pages/GiftCards';
 import Footer from './components/Footer';
+import { AnimatePresence, motion } from 'framer-motion';
+
+export function AnimatedRoutes(){
+
+  const location = useLocation();
+
+  return(
+    <AnimatePresence mode='wait'>
+      <Routes location={location} key={location.pathname}>
+        <Route
+         path="/"
+          element={<PageWrapper><Home/></PageWrapper>} />
+        <Route
+         path="/menu"
+          element={<PageWrapper><Menu/></PageWrapper>} />
+        <Route
+         path="/ourstory"
+          element={<PageWrapper><OurStory /></PageWrapper>} />
+        <Route
+         path="/giftcards"
+         element={<PageWrapper><GiftCards /></PageWrapper>} />
+        <Route
+         path="/contact"
+          element={<PageWrapper><Contact /></PageWrapper>} />
+      </Routes>
+    </AnimatePresence>
+  )
+
+}
+
+interface PageWrapperProps {
+  children: React.ReactNode;
+}
+
+export function PageWrapper({children}: PageWrapperProps){
+  return (
+    <motion.div
+     initial={{ opacity: 0, y:20}}
+      animate={{ opacity: 1, y:0 }}
+       exit={{ opacity: 0, y:-20 }}
+       transition={{duration: .4}}>
+        {children}
+    </motion.div>
+  )
+}
 
 
 function App() {
@@ -16,13 +61,7 @@ function App() {
     <div className="App flex flex-col min-h-screen">
       <NavBar />
     <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/ourstory" element={<OurStory />} />
-        <Route path="/giftcards" element={<GiftCards />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+    <AnimatedRoutes />
     </div>
     <Footer />
     </div>
